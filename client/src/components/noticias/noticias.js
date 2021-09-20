71faab307ef48d0b1e125128c557205770f6c11d
SMNahuel <nahuelsan96@gmail.com>
2021-09-20 20:20:21 -0300

    feat: noticies-author

--- client/src/components/noticias/noticias.js
+++ client/src/components/noticias/noticias.js
@@ -3,9 +3,10 @@ import axios from "axios";
 import style from "./noticias.module.css";
 
 const Noticias = (post) => {
-  const [state, setState] = useState("");
-
-  const [author, setAuthor] = useState("");
+  const [state, setState] = useState({
+    post: [""],
+    author: "",
+  });
 
   useEffect(() => {
     fetchData();
@@ -16,36 +17,24 @@ const Noticias = (post) => {
     const post = await axios
       .get(`http://localhost:5000/post/${id}`)
       .then(({ data }) => {
-        return data[0];
-      });
-
-    setState(post);
-
-    await fetchUser();
-  };
-
-  const fetchUser = async () => {
-    const author = await axios
-      .get(`http://localhost:5000/user/${state.userId}`)
-      .then(({ data }) => {
         return data;
       });
 
-    setAuthor(author);
+    setState(post);
   };
 
   return (
     <div className={style.Container}>
       <div className={style.Notice}>
-        <div className={style.Title}>{state.post_title}</div>
+        <div className={style.Title}>{state.post[0].post_title}</div>
         <div className={style.ContainerPhoto}>
           <img src={post.Img} className={style.PhotoNotice} />
         </div>
-        <div className={style.Content}>{state.post_text}</div>
+        <div className={style.Content}>{state.post[0].post_text}</div>
       </div>
       <div className={style.Author}>
         <div className={style.PhotoAutor}></div>
-        <h4 className={style.About}>Nunito</h4>
+        <h4 className={style.About}>{state.author.name}</h4>
         <h4 className={style.About}>Acerca de: </h4>
         <div className={style.AboutAuthor}>
           Lorem Ipsum is simply dummy text of the printing and typesetting
